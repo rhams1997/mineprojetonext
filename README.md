64256b43a9f08c0688c73c5077afb8982b2a938a
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2022-04-05 18:53:30 -0300

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Criando Portfolio com Next.JS
+
+Desenvolvimento WEB com o framework Next.JS - Criação de um Portfolio.
+
+**Integrantes do grupo:** Luis Gustavo, Rafael Henrique e Yan Silveira.
---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import '../../styles/globals.css'
+import Head from 'next/head'
+import Header from '../../components/Header'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+    <Head>
+      <title>Portfólio...</title>
+    </Head>
+    <Header />
+    <Component { ... pageProps} />
+
+    </>
+  )
+}
+
+export default MyApp
